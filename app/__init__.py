55463feb8e1f45f91c69867caa147a5d0fc3dab6
olamideabiodun <116852940+olamideabiodun@users.noreply.github.com>
2024-06-18 13:09:47 +0300

    Database connected

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from config import configur
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(configur)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes
+from app import routes, models
--- config.py
+++ config.py
@@ -1,3 +1,8 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class configur:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Lets try this'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    'sqlite:///' + os.path.join(basedir, 'app.db')
+
